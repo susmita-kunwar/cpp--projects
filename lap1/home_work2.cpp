c88b6bacae7263a1bbf51e763dd895382d47367b
Susmita Kunwar <shusmitakunwar@gmail.com>
2021-05-18 22:33:04 +0545

    home_work.cpp

--- lap1/home_work2.cpp
+++ lap1/home_work2.cpp
@@ -2,12 +2,15 @@
 #include <math.h>
 using namespace std;
 int main() { 
-    int a,b,c;
+    int a,b,c,x;
     float s,area;
     cout <<" enter a,b,c and s:";
-    cin >> a>> b>> c;
+    cin >> a;
+    cin >>b;
+    cin >>c;
     s=(a+b+c)/2;
-    area=sqrt(s*(s-a)*(s-b)*(s-c));
+    x=(s*(s-a)*(s-b)*(s-c));
+    area=pow(x,o.5);
     cout << "area of triangle is:"<<area;
     return 0;
 }
