f27c12d9524d27fd548f07b41f016c6fc557792e
Susmita Kunwar <shusmitakunwar@gmail.com>
2021-05-19 13:13:46 +0545

    .\lap1_area_of_triangle\

--- lap1_area_of_triangle/home_work2.cpp
+++ lap1_area_of_triangle/home_work2.cpp
@@ -6,11 +6,14 @@ int main() {
     float s,area;
     cout <<" enter a,b,c and s:";
     cin >> a;
-    cin >>b;
-    cin >>c;
+    cout << "enter value of a:";
+    cin >> b;
+    cout << "enter value of b:";
+    cin >> c;
+    cout << "enter value of c:";
     s=(a+b+c)/2;
     x=(s*(s-a)*(s-b)*(s-c));
-    area=pow(x,o.5);
+    area=pow(x,0.5);
     cout << "area of triangle is:"<<area;
     return 0;
 }
