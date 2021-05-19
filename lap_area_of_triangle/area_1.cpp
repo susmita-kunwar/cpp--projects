e3d50d8bbf9525a120c27a937cd021bf1a878203
Susmita Kunwar <shusmitakunwar@gmail.com>
2021-05-19 14:13:10 +0545

    lap_area_of_triangle

---
+++ lap_area_of_triangle/area_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+    int b,h;
+    float area;
+    cout <<"enter b and h:";
+    cin >> b >> h;
+    area = 0.5*b*h;
+    cout << "area of triangle is:"<<area;
+    return 0;
+}
