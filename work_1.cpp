261e17bb2250a1530331c71ce3c57d188162a8e5
Susmita Kunwar <shusmitakunwar@gmail.com>
2021-05-31 17:57:59 +0545

    .\work_1.cpp

---
+++ work_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "enter positive number:" <<endl;
+    cin>> num;
+    if (num >0) {
+        cout << "positive number:" << num << endl;
+    }
+
+        cout << "this statement is always executed";
+   return 0;
+}
+
