5ce992174ff5c306008b534fdb16982e08b9fe14
Susmita Kunwar <shusmitakunwar@gmail.com>
2021-05-31 22:01:22 +0545

    .\work1.cpp

---
+++ assignment_work/work1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+ 
+ int main() {
+    float num;
+    cout  << "enter the integer"<< endl;
+    cin >> num;
+    if (num>=0)
+    {
+        cout << "positive number"<< num << endl;
+    }
+     cout << "enter number is valide";
+ }
+
