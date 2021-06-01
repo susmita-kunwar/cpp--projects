a27a2284c821a68949537dc41307debde8912146
Susmita Kunwar <shusmitakunwar@gmail.com>
2021-06-01 10:39:53 +0545

    .\work5.cpp

---
+++ assignment_work/work5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+ 
+ int main() {
+    float num1,num2;
+    char op;
+    cout  << "enter operators '+' , '-' , '*' , '%' :"<< endl;
+    cin >> op;
+    cout  <<"enter the operands :";
+    cin >> num1 >> num2 ;
+
+    switch (op){
+        case '+':
+         cout << num1 << "+"<< num2 << "=" << num1 + num2 ;
+           break;
+
+        case '-':
+          cout << num1 <<  "-" << num2 << "=" << num1 - num2 ;
+           break;
+
+        case '*':
+          cout << num1 << "*" << num2 << "=" << num1 * num2 ;
+           break;
+
+        case '/':
+          cout << num1 << "/" << num2 << "/" << num1 / num2 ;
+           break;
+
+        defalt:
+         cout << "error or invalide" << endl;
+          break;
+    }
+ return 0;
+           }
+
