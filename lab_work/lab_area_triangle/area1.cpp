4fca0bd12ba676932f6ae71def676ccc04993a07
Susmita Kunwar <shusmitakunwar@gmail.com>
2021-06-02 20:58:28 +0545

    .\area1.cpp.cpp

---
+++ lab_work/lab_area_triangle/area1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+int main(){
+    int a,b,c,x;
+    float area,s;
+    cout <<"enter the sides" << endl;
+   cout <<"enter side a:";
+    cin >> a;
+    cout <<"enter side b:";
+    cin >> b;
+    cout<<"enter side c:";
+    cin >> c;
+    s=(a+b+c)/2;
+    x=(s*(s-a)*(s-b)*(s-c));
+    area=pow(x,0.5);
+    cout << "area of triangle is:"<<area;
+    return 0;
+}
