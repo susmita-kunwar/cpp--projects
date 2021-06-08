0a6ff6f02b19d5cdef9b1a3a6d69ade69b6d5119
Susmita Kunwar <shusmitakunwar@gmail.com>
2021-06-08 06:13:51 +0545

    determine length of string

---
+++ lab_work/library_function/strlen_function.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+# include <string.h>
+using namespace std;
+int main()
+{
+    char name [200];
+    int length;
+    cout << "enter the string:"<< endl;
+    cin >> name;
+    length = strlen(name);
+    cout <<"enter length:" << length << endl;
+    return 0;
+
+}
+
+
