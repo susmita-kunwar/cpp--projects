4712aa6b12198c23149d701f30d87afe7458b1d6
Susmita Kunwar <shusmitakunwar@gmail.com>
2021-07-24 09:41:45 +0545

    Function template

---
+++ lab_work/Template/template1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+
+T Large(T n1, T n2)
+{
+    return (n1 > n2) ? n1:n2;
+
+}
+
+int main()
+{
+    int i1 ,i2;
+    float f1, f2;
+    char c1, c2;
+
+    cout <<"enter two integer is :";
+    cin >>i1>>i2;
+    cout <<Large(i1, i2) <<"is large:"<<endl;
+
+    cout <<"enter two float is :";
+    cin >>f1>>f2;
+    cout <<Large(f1, f2) <<"is large:"<<endl;
+
+    cout <<"enter two character is :";
+    cin >>c1>>c2;
+    cout <<Large(c1, c2) <<"is large:"<<endl;
+
+    return 0;
+}
+
