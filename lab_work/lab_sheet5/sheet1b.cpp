fc5595950766ab303b0ff867ccc996fd60483e08
Susmita Kunwar <shusmitakunwar@gmail.com>
2021-07-27 18:00:41 +0545

    operator overloadig in postfix

---
+++ lab_work/lab_sheet5/sheet1b.cpp
@@ -0,0 +1,44 @@
+# include <iostream>
+using namespace std;
+
+class Fibonacci 
+{
+    int a,b,c;
+        public:
+            Fibonacci()
+                {
+                    a = 0;
+                    b = 1;
+                    c = 0;
+                }
+            /*void operator++()     // Overload ++ when used as prefix
+            {
+                cout << a <<" ,";
+                c = a+b;
+                a = b;
+                b = c;
+            }               */
+
+            void operator++(int)     // Overload operator when used as postfix
+            {
+                cout << a <<" ,";
+                c = a+b;
+                a = b;
+                b = c;
+            }                   
+};
+
+int main()
+{
+    Fibonacci f;
+    int i,n;
+    cout<<"\n enter the number of n:"<<endl;
+    cin >>n;
+
+    for(int i = 1; i <=n; i++)
+    {
+        f++;
+    }
+    return 0;
+
+}
